bf2c2f7b27f1825f7490ae65e632f806be320ab5
tkuramot <mabdman512@gmail.com>
2026-04-24 02:49:32 +0900

    Add nix devShell with Go, Node.js 24 LTS, TypeScript

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "fumi development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gopls
+            pkgs.gotools
+            pkgs.go-tools
+
+            pkgs.nodejs_24
+            pkgs.typescript
+
+            pkgs.jq
+          ];
+
+          shellHook = ''
+            echo "fumi devShell: $(go version), node $(node --version), tsc $(tsc --version)"
+          '';
+        };
+      }
+    );
+}
